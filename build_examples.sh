999b21b4e814066fbd73fcfbd06c1488f915772e
Forrest York <forrest@eyesopen.com>
2017-11-15 16:49:34 -0700

    Updates build and run scripts
    Also adds in the Docker file for chalice, runs on only 1 core

--- build_examples.sh
+++ build_examples.sh
@@ -4,4 +4,5 @@ docker build ./ -f ./Dockerfile-base -t example-base:latest
 docker build ./ -f ./Dockerfile-python -t python-example
 docker build ./ -f ./Dockerfile-flask -t flask-example
 docker build ./ -f ./Dockerfile-django -t django-example
+docker build ./ -f ./Dockerfile-chalice -t chalice-example
 
